e45f06fb9ff1d42ce2a6345dc633bdcb95418ee4
root <root@user1>
2026-03-01 04:11:12 +0300

    Remove WalletConnect, use relative paths for GitHub Pages

--- assets/vendor-g-mxytbU.js
+++ assets/vendor-g-mxytbU.js
@@ -1,4 +1,4 @@
-import{r as _a}from"./web3-CMFeTdw6.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{r as _a}from"./web3-XmhQtPdq.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
